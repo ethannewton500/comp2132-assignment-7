80d6acb2610c55e88c08d2aa641dc24474e43c0f
ethannewton500 <109337928+ethannewton500@users.noreply.github.com>
2023-10-29 20:48:25 -0700

    apply google styles to js

--- js/script.js
+++ js/script.js
@@ -1,83 +1,82 @@
+/* eslint-disable require-jsdoc */
 /**
  * The following code is to calculate the price
  */
 
-const displayPrice = $("#displayPrice")
-const displayTotal = $("#displayTotal")
+const displayPrice = $('#displayPrice');
+const displayTotal = $('#displayTotal');
 
-const userQuantity = $('#quantity')
+const userQuantity = $('#quantity');
 
-const price = 20
-const total = userQuantity.val() * price
+const price = 20;
+const total = userQuantity.val() * price;
 
 
-displayPrice.text(price + ".00")
-displayTotal.text(total + ".00")
+displayPrice.text(price + '.00');
+displayTotal.text(total + '.00');
 
 function calculateTotal( event ) {
-    const quantity = event.target.value
-    const total = quantity * price
+  const quantity = event.target.value;
+  const total = quantity * price;
 
-    displayTotal.text(total + ".00")
+  displayTotal.text(total + '.00');
 }
-userQuantity.on("change", calculateTotal)
+userQuantity.on('change', calculateTotal);
 
 /**
- * The following code handles the color selctions and will also change the images addording to color
+ * The following code handles the color selctions and will
+  also change the images addording to color
  */
 
-const black = $("#black")
-const red = $("#red")
-const grey = $("#grey")
+const black = $('#black');
+const red = $('#red');
+const grey = $('#grey');
 
-const thumb1 = $("#thumb1")
-const thumb2 = $("#thumb2")
-const thumb3 = $("#thumb3")
+const thumb1 = $('#thumb1');
+const thumb2 = $('#thumb2');
+const thumb3 = $('#thumb3');
 
-const mainImage = $("#mainImage")
+const mainImage = $('#mainImage');
 
 function changeImagesColor( event ) {
+  let mainImageView = $('#mainImage').attr('src').toString();
 
-  let mainImageView = $("#mainImage").attr("src").toString()
+  mainImageView = mainImageView.split('-').pop(-1);
 
-  mainImageView = mainImageView.split("-").pop(-1);
-  
-  if (mainImageView === "model.jpg") {
-    mainImageView = "no-model.jpg"
+  if (mainImageView === 'model.jpg') {
+    mainImageView = 'no-model.jpg';
   }
 
 
-  if (black.is(":checked")) {
-    mainImage.attr("src", "product-images/t-shirt-black-" + mainImageView)
-    thumb1.attr("src", "product-images/t-shirt-black-no-model.jpg")
-    thumb2.attr("src", "product-images/t-shirt-black-front.jpg")
-    thumb3.attr("src", "product-images/t-shirt-black-back.jpg")
-  } else if (red.is(":checked")) {
-    mainImage.attr("src", "product-images/t-shirt-red-" + mainImageView)
-    thumb1.attr("src", "product-images/t-shirt-red-no-model.jpg")
-    thumb2.attr("src", "product-images/t-shirt-red-front.jpg")
-    thumb3.attr("src", "product-images/t-shirt-red-back.jpg")
-  } else if (grey.is(":checked")) {
-    mainImage.attr("src", "product-images/t-shirt-grey-" + mainImageView)
-    thumb1.attr("src", "product-images/t-shirt-grey-no-model.jpg")
-    thumb2.attr("src", "product-images/t-shirt-grey-front.jpg")
-    thumb3.attr("src", "product-images/t-shirt-grey-back.jpg")
+  if (black.is(':checked')) {
+    mainImage.attr('src', 'product-images/t-shirt-black-' + mainImageView);
+    thumb1.attr('src', 'product-images/t-shirt-black-no-model.jpg');
+    thumb2.attr('src', 'product-images/t-shirt-black-front.jpg');
+    thumb3.attr('src', 'product-images/t-shirt-black-back.jpg');
+  } else if (red.is(':checked')) {
+    mainImage.attr('src', 'product-images/t-shirt-red-' + mainImageView);
+    thumb1.attr('src', 'product-images/t-shirt-red-no-model.jpg');
+    thumb2.attr('src', 'product-images/t-shirt-red-front.jpg');
+    thumb3.attr('src', 'product-images/t-shirt-red-back.jpg');
+  } else if (grey.is(':checked')) {
+    mainImage.attr('src', 'product-images/t-shirt-grey-' + mainImageView);
+    thumb1.attr('src', 'product-images/t-shirt-grey-no-model.jpg');
+    thumb2.attr('src', 'product-images/t-shirt-grey-front.jpg');
+    thumb3.attr('src', 'product-images/t-shirt-grey-back.jpg');
   }
 
-  console.log(mainImage.attr("src"))
+  console.log(mainImage.attr('src'));
 }
-
-const view = $("#subImages")
 
 function changeMainImage( event ) {
-  const image = event.target.src
-  mainImage.attr("src", image)
+  const image = event.target.src;
+  mainImage.attr('src', image);
 }
 
-black.on("change", changeImagesColor)
-red.on("change", changeImagesColor)
-grey.on("change", changeImagesColor)
+black.on('change', changeImagesColor);
+red.on('change', changeImagesColor);
+grey.on('change', changeImagesColor);
 
-thumb1.on("click", changeMainImage)
-thumb2.on("click", changeMainImage)
-thumb3.on("click", changeMainImage)
+thumb1.on('click', changeMainImage);
+thumb2.on('click', changeMainImage);
+thumb3.on('click', changeMainImage);
